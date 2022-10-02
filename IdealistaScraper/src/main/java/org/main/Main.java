5c4a75814f5967bdbc22e8f8efec7c51a25181a4
ErnestCV <107429970+ErnestCV@users.noreply.github.com>
2022-10-02 18:45:16 +0200

    Add files via upload

---
+++ IdealistaScraper/src/main/java/org/main/Main.java
@@ -0,0 +1,78 @@
+package org.main;
+
+import com.gargoylesoftware.htmlunit.BrowserVersion;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+
+    private static final int ELEMENTS_PER_PAGE = 30;
+
+    public static void main(String[] args) {
+
+        //URL de la localització
+        String url = "https://www.idealista.com/ca/alquiler-viviendas/barcelona/eixample/el-fort-pienc/";
+        HtmlPage page = getDocument(url);
+
+        //Obtenim el nombre d'habitatges disponibles a la zona
+        String numberSelector = "//h1[@id=\"h1-container\"]/text()";
+        List<Object> totalTemp = page.getByXPath(numberSelector);
+        double total = objectToDouble(totalTemp.get(0));
+
+        //Obtenim el llistat de preus
+        List<Double> prices = new ArrayList<>();
+        String priceSelector = "//span[@class=\"item-price h2-simulated\"]/text()";
+
+        //Trobem els preus de la primera pàgina
+        List<Object> firstPageElements = page.getByXPath(priceSelector);
+        firstPageElements.forEach(price ->
+                prices.add(objectToDouble(price)));
+
+        //Iterem per totes les altres pàgines començant per la segona
+        for (int i = 2; i <= Math.ceil(total / ELEMENTS_PER_PAGE); i++) {
+            String formattedUrl = String.format(url + "pagina-%d.htm", i);
+            HtmlPage tempPage = getDocument(formattedUrl);
+            List<Object> currentPageElements = tempPage.getByXPath(priceSelector);
+            currentPageElements.forEach(price ->
+                    prices.add(objectToDouble(price)));
+        }
+
+        //Comprovem el nombre d'habitatges
+        System.out.println(prices.size());
+
+        //Mitjana de preus
+        System.out.println(prices.stream().mapToDouble(a -> a).average().orElse(0));
+
+        //Mediana de preus
+        Collections.sort(prices);
+        System.out.println(percentile(prices, 50));
+    }
+
+    public static HtmlPage getDocument(String url) {
+        HtmlPage page = null;
+        try (final WebClient webClient = new WebClient(BrowserVersion.FIREFOX)) {
+            webClient.getOptions().setCssEnabled(false);
+            webClient.getOptions().setJavaScriptEnabled(false);
+            page = webClient.getPage(url);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return page;
+    }
+
+    //Obtenim preu en double a partir de les dades retornades
+    public static double objectToDouble(Object price) {
+        return Double.parseDouble(price.toString().split(" ")[0].replace(".", ""));
+    }
+
+    //Percentils
+    public static Double percentile(List<Double> prices, double percentile) {
+        int index = (int) Math.ceil(percentile / 100.0 * prices.size());
+        return prices.get(index - 1);
+    }
+}
